0478147f38a6f671a503b73912d031d9ba417d1a
rayne <pothong.r@gmail.com>
2025-09-23 22:18:34 -0700

    added plus minus

--- plusminus.js
+++ plusminus.js
@@ -2,5 +2,42 @@
 
 function plusMinusSign(str){
     let sign = ""
-    let splitString = str.split()
+    for (let i = 0; i < str.length; i++){
+        if (i <= str.length - 5 && str.slice(i, i + 5) === 'minus'){
+            sign += '-'
+            i += 4
+        }else if (i <= str.length - 4 && str.slice(i, i + 4) === 'plus'){
+            sign += '+'
+            i += 3
+        }
+    }
+    return sign
 }
+
+console.log(plusMinusSign('minusplusplusminusminus')) // "-++--"
+console.log(plusMinusSign('plusminusplus')) // "+-+"
+console.log(plusMinusSign('minusminusminus')) // "---"
+
+// while loop
+function extractSigns(str) {
+    let result = '';
+    let i = 0;
+    
+    while (i < str.length) {
+        if (str.substring(i, i + 5) === 'minus') {
+            result += '-';
+            i += 5; // Skip past "minus"
+        } else if (str.substring(i, i + 4) === 'plus') {
+            result += '+';
+            i += 4; // Skip past "plus"
+        } else {
+            i++; // Move to next character if no match
+        }
+    }
+    
+    return result;
+}
+
+console.log(extractSigns("minusplusplusminusminus")); // "-++--"
+console.log(extractSigns("plusminusplus")); // "+-+"
+console.log(extractSigns("minusminusminus")); // "---"
